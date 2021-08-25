58cd17de6e2767fdad113ebca591e3d842eb799f
Krzysztof Bannach <krzysztof92b@gmail.com>
2021-08-25 22:59:01 +0200

    working Google Maps Directions API demo

--- Vagrantfile
+++ Vagrantfile
@@ -2,10 +2,14 @@ $script = <<-SCRIPT
 sudo apt-get update
 sudo apt-get install --assume-yes python-pip
 pip install flask
+pip install -U googlemaps
 export FLASK_APP=flaskr
 export FLASK_ENV=development
+export APP_CONFIG_FILE=/workspace/flaskr/config.cfg
 # use this alias to start a flask application server with the access from the host machine
-alias frun="flask run --host=192.168.50.4"
+alias frun='flask run --host=192.168.50.4' >> /home/vagrant/.bash_aliases
+echo "alias frun='flask run --host=192.168.50.4'" >> /home/vagrant/.bash_aliases
+chown vagrant /home/vagrant/.bash_aliases
 SCRIPT
 
 Vagrant.configure("2") do |config|
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,20 +1,16 @@
 import os
 
 from flask import Flask
+from . import config
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # defaults, can be overriden below
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_object(config.DevelopmentConfig)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -29,5 +25,29 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    # directions endpoint
+    import googlemaps
+    from datetime import datetime
+    gmaps = googlemaps.Client(key=app.config['GOOGLE_API_KEY'])
+
+    from . import directions
+    app.register_blueprint(directions.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    from flask import render_template
+
+    @app.route('/directions')
+    def directions():
+        now = datetime.now()
+        res = gmaps.directions("Gdynia",
+                         "Gdansk",
+                         mode="transit",
+                         departure_time=now)
+        startingPoint=res[0]['legs'][0]['start_location']
+        endPoint=res[0]['legs'][0]['end_location']
+        totalDist=res[0]['legs'][0]['distance']['text']
+        totalTime=res[0]['legs'][0]['duration']['text']
+        return render_template('directions/directions.html', startingPoint=startingPoint, endPoint=endPoint, totalDist=totalDist, totalTime=totalTime)
 
     return app
